2fd3bf2395170dd5ffee4a7c866610eb3063188c
Nasef161995~ <mnasef905@gmail.com>
2023-11-07 00:36:01 +0300

    aaaa

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python3
-"""
-"""
+"""......."""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """......."""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        # self.name = name
+        # self.my_number = my_number
+    
+
+    def __str__(self):
+        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+    
+    def save(self):
+         self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        new = self.__dict__
+        new["__class__"] = self.__class__.__name__
+        created_at = created_at.isoformat()
+        return new 
+
+
+
+a = BaseModel()
+print(a)
+a.created_at = 10
+a.save()
+
+print(a)
---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+
+m = now.isoformat()
+
+print(now)
+print(m)
