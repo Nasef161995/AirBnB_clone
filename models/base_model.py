0a52b4b30c8f55e9cca6fabec32c273516f95425
Nasef161995~ <mnasef905@gmail.com>
2023-11-11 21:17:07 +0300

    1111

---
+++ models/base_model.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""BaseModel of the AirBnB clone project
+"""
+
+from datetime import datetime
+import uuid
+from models import storage
+
+
+
+class BaseModel:
+    """Base model of the project
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Constructor  of the class
+        Args:
+            *args (any): arguments.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    format_string = "%Y-%m-%dT%H:%M:%S.%f"
+                    self.__dict__[key] = datetime.strptime(
+                        value, format_string)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """print representation of the class
+        """
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute
+        """
+
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        """
+
+        new = self.__dict__.copy()
+        new["__class__"] = self.__class__.__name__
+        new["created_at"] = self.created_at.isoformat()
+        new["updated_at"] = self.updated_at.isoformat()
+        return new
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,20 +5,20 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """serializes instances to a JSON file
+    """serializes instances to a JSON file 
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """ returns the dictionary __objects"""
-        return FileStorage.__objects
+        return FileStorage.__objects 
 
     def new(self, obj):
         """sets in __objects the obj with key"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
-
+    
     def save(self):
         """serializes __objects to the JSON file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file_json:
@@ -26,7 +26,7 @@ class FileStorage:
             for key, value in FileStorage.__objects.items():
                 d[key] = value.to_dict()
             json.dump(d, file_json)
-
+    
     def reload(self):
         """ deserializes the JSON file to __objects"""
         try:
@@ -35,5 +35,5 @@ class FileStorage:
                 data = json.load(file_json)
                 for value in data.values():
                     self.new(eval(value['__class__'])(**value))
-        except BaseException:
-            pass
+        except:
+            pass    
