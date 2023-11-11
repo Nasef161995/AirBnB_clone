1bba878e19835d97ef20c01fc7c3237d993ae40b
MonaaEid <monaa_eid@hotmail.com>
2023-11-11 19:59:54 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,9 @@
 """BaseModel of the AirBnB clone project
 """
 
-# import models
+import models
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -33,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """print representation of the class
@@ -46,7 +45,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
