023b229d62f9d16c6f25ac6ea036d9e93d1eed1d
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-11-12 20:03:25 +0200

    Add files via upload

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Unittest for User([..])
+"""
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """ Unittest for User"""
+
+    def setUp(self):
+        """ test"""
+
+        self.user = User()
+
+    def test_user_email(self):
+        """ email test"""
+
+        self.assertEqual(self.user.email, "")
+
+    def test_user_password(self):
+        """ password test"""
+
+        self.assertEqual(self.user.password, "")
+
+    def test_user_first_name(self):
+        """ first_name test"""
+
+        self.assertEqual(self.user.first_name, "")
+
+    def test_user_last_name(self):
+        """ first_name test"""
+
+        self.assertEqual(self.user.last_name, "")
+
+if __name__ == "__main__":
+    unittest.main()
