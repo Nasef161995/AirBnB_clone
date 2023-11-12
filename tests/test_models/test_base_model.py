ea7f77e834d1d1c3b45d236364403db2d36b2f0c
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-11-12 18:17:23 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,9 @@ import unittest
 from models.base_model import BaseModel
 
 
-
 class BaseModelTests(unittest.TestCase):
+    """Test BaseModel class"""
+
     def test_init(self):
         data = {"name": "John", "age": 30}
         model = BaseModel(**data)
@@ -16,5 +17,27 @@ class BaseModelTests(unittest.TestCase):
         self.model = BaseModel()
 
     def test_save(self):
+        """Test the save method of the BaseModel class"""
+
         self.model.save()
         self.assertTrue(self.model.id is not None)
+    
+    def test_to_dict(self):
+        """Test the to_dict method of the BaseModel class"""
+
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn('id', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+
+    def test_str(self):
+        """Test the __str__ method of the BaseModel class"""
+        model = BaseModel()
+        model_str = str(model)
+        self.assertIsInstance(model_str, str)
+        self.assertIn('BaseModel', model_str)
+        self.assertIn('id', model_str)
+        self.assertIn('created_at', model_str)
+        self.assertIn('updated_at', model_str)    
