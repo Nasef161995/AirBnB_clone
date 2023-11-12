27cd771a4d3ad95a9503cbdee0ca0bff3868783e
Nasef161995~ <mnasef905@gmail.com>
2023-11-13 00:05:56 +0300

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,26 +14,64 @@ import json
 class FileStorageTest(unittest.TestCase):
     """unittest for file storage"""
 
+    def all_method_test(self):
+        """test all method"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertEqual(type(obj), dict)
+        self.assertIsNotNone(obj)
+        self.assertIs(obj, storage._FileStorage__objects)
+
+    def attr_test(self):
+        """test of attrs"""
+
+        self.assertTrue(hasattr(FileStorage, '_FileStorage__objects'))
+        self.assertTrue(hasattr(FileStorage, '_FileStorage__file_path'))
+
+    def new_method_test(self):
+        """test new method """
+
+        storage = FileStorage()
+        obj = storage.all()
+        user = User()
+        user.id = 54791156
+        user.name = "mohamed"
+        storage.new(user)
+        val = user.__class__.__name__ + "." + str(user.id)
+        self.assertIsNotNone(obj[val])
+
+    def save_test(self):
+        """test save method"""
+
+        fun = self._model.to_dict()
+        val = fun['__class__'] + "." + fun['id']
+        storage = FileStorage()
+        storage.save()
+        with open("file.json", 'r') as f:
+            data = json.load(f)
+        new = data[val]
+        for key in new:
+            self.assertEqual(fun[key], new[key])
+
     def tests(self):
         """test cases"""
+        model = BaseModel()
+        model.name = "My_First_Model"
+        model.my_number = 89
+        model.save()
 
-        all_objs = storage.all()
-        for obj_id in all_objs.keys():
-            obj = all_objs[obj_id]
+        objs = storage.all()
+        for id in objs.keys():
+            obj = objs[id]
 
         try:
             with open("file.json", "r", encoding='utf-8') as f:
-                self.assertIsInstance(all_objs, dict)
+                self.assertIsInstance(objs, dict)
                 self.assertIsInstance(obj, BaseModel)
                 val = f"[{obj.__class__.__name__}] ({obj.id}) {obj.__dict__}"
                 self.assertEqual(str(obj), val)
-                self.assertEqual(f"{obj_id}",
+                self.assertEqual(f"{id}",
                                  f"{obj.__class__.__name__}.{obj.id}")
         except FileNotFoundError:
-            self.assertIsInstance(all_objs, dict)
-            self.assertEqual(all_objs, {})
-
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        my_model.save()
+            self.assertIsInstance(objs, dict)
+            self.assertEqual(objs, {})
